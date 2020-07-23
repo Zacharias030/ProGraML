2981f8dc20ab456756149ee3aeca7c6cdfd51038
Chris Cummins <chrisc.101@gmail.com>
2020-07-23 19:25:59 +0100

    Add a param to nx2data() to use ablated vocab.

--- deeplearning/ml4pl/poj104/dataset_test.py
+++ deeplearning/ml4pl/poj104/dataset_test.py
@@ -13,5 +13,11 @@ print("Loaded", len(vocab), "element vocabulary")
 data = dataset.nx2data(graph, vocab)
 print("Created unlabelled graph data", data)
 
+data = dataset.nx2data(graph, vocab, ablate_vocab=dataset.AblationVocab.NO_VOCAB)
+print("Ablated vocab", data)
+
+data = dataset.nx2data(graph, vocab, ablate_vocab=dataset.AblationVocab.NODE_TYPE_ONLY)
+print("Ablated node type", data)
+
 data = dataset.nx2data(graph, vocab, "poj104_label")
 print("Created POJ-104 labelled graph data", data)
